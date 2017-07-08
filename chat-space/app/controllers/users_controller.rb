b75fc62226e77479179aa8745c3cd1b7740810ff
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-08 20:12:38 +0900

    get user-infomation

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def edit
-    @user = User.find(params[:id])
+  user = current_user
   end
 
   def update
-    if curretnt_user.update(user_params)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render 'edit'
