fac55e21008eea1e16de48bfb20b85053b53d869
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-08 15:13:55 +0900

    Edit-action,Update-action

---
+++ chat-space/app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+  def edit
+
+  end
+
+  def update
+    if curretnt_user.update(user_params)
+      redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
