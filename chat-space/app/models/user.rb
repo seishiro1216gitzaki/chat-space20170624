c3b98de55d6d00c60e72ff5c009628205dc182a2
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-15 13:03:04 +0900

    Edit models

--- chat-space/app/models/user.rb
+++ chat-space/app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence :true, uniqueness: true
+  validates :email, presence :true, uniqueness: true
+  has_many :groups, through: members
+  has_many :messages
+  has_many :members
 end
