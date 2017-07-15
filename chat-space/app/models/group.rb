7d6872328ae253902b3280a11b923d3c205a097e
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-15 18:30:37 +0900

    Edit-group-model

--- chat-space/app/models/group.rb
+++ chat-space/app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  validates :name, presence :true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   has_many :members
-  has_many :users, through: members
+  has_many :users, through: :members
   has_many :messages
 end
