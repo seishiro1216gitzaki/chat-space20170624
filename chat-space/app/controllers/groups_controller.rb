6f2bd2f32d206662bd6cce891d6b62f4f2df81ce
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-15 18:30:10 +0900

    Edit-groups-controller

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,10 +1,15 @@
 class GroupsController < ApplicationController
   def new
-
+    @group = Group.new
   end
 
   def create
-
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -13,5 +18,10 @@ class GroupsController < ApplicationController
 
   def update
 
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
