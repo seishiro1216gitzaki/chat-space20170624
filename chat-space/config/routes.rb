6276c2736e4c2c589c461e86b5aa3fbcaaf7d9a3
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-19 20:14:54 +0900

    groups-nest-to-messages

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, except: [:show, :destroy]
+  resources :groups, except: [:show, :destroy] do
+    resources :messages,only: :index
+  end
 end
