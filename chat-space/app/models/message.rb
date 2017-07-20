45213d7bc3b752d465eee4c9533d3005c6e6aab2
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-20 22:13:27 +0900

    Edit-message-model

--- chat-space/app/models/message.rb
+++ chat-space/app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :user_id, presense: true
-  validates :group_id, presence: true
+  validates :body, presence: true, if: -> { image.blank? }
+  validates :image, presence: true, if: -> { body.blank? }
 end
