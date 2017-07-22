88585b8bf24309fb3efc389057265ba4acc8eee3
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-22 15:12:05 +0900

    アクションの大枠を作成

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,3 +1,17 @@
 class MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
+  end
 
+  def create
+    @group = Group.find(params[:group_id])
+    @message = Message.new(create_params)
+  end
+
+
+  private
+  def create_params
+    params.require(:message).permit(:body, :image).merge(user_id: current.user_id, group_id: params[:group_id])
+  end
 end
