27b54a4d34e3f8aa9e527156108b58211a9799af
seishiro1216gitzaki <canoe1216@gmail.com>
2017-07-22 15:26:31 +0900

    Edit-messages-controller

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,17 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_group only: [:index, :create]
   def index
-    @group = Group.find(params[:group_id])
     @message = Message.new
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = Message.new(create_params)
   end
 
-
   private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
   def create_params
     params.require(:message).permit(:body, :image).merge(user_id: current.user_id, group_id: params[:group_id])
   end
+
 end
