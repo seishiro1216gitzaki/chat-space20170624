5f91dab9fc729fa9e422ded58748a21af0cf34a7
seishiro1216gitzaki <canoe1216@gmail.com>
2017-08-02 20:52:18 +0900

    password_written_by_faker

--- chat-space/spec/factories/users.rb
+++ chat-space/spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
-
+  pass = Faker::Internet.password(8)
   factory :user do
     id                    1
     name                  Faker::Name.name
     email                 Faker::Internet.email
-    password              "11111111"
-    password_comfirmation "11111111"
+    password              pass
+    password_comfirmation pass
   end
 
 end
