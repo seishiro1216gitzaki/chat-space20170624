8a3df471201f04b6ac2637a110f06b7e6279fee3
seishiro1216gitzaki <canoe1216@gmail.com>
2017-08-05 19:37:41 +0900

    modified-messages_controller_spec.rb

--- chat-space/spec/controllers/messages_controller_spec.rb
+++ chat-space/spec/controllers/messages_controller_spec.rb
@@ -16,44 +16,44 @@ describe MessagesController do
         end
         # アクション内で定義しているインスタンス変数があるか
         it 'assigns the requested message to @message' do
-         expect(assigns(:message)).to be_a_new Message
+          expect(assigns(:message)).to be_a_new Message
         end
         # アクション内で定義しているインスタンス変数があるか
         it 'assigns the requested group to @group' do
-         expect(assigns(:group)).to eq group
+          expect(assigns(:group)).to eq group
         end
         # 該当するビューが描画されているか
         it "renders the :index template" do
-         get :index, params: {group_id: group}
-         expect(response).to render_template :index
+          get :index, params: {group_id: group}
+          expect(response).to render_template :index
         end
       end
-      # ログインしていない場合
+# ログインしていない場合
       context 'user signed_out' do
         before do
           get :index, params: {group_id: group}
         end
         # 意図したビューにリダイレクトできているか
         it "redirect to the view if signed_out" do
-         expect(response).to render_template :index
+          expect(response).to render_template :index
         end
       end
     end
 # メッセージを作成するアクション
     describe 'POST #create' do
 # ログインしているかつ、保存に成功した場合
-     context 'user signed_in and saved message' do
+      context 'user signed_in and saved message' do
         before do
           login_user user
         end
         # メッセージの保存はできたのか
         it 'saved message' do
-         expect{post :create, params: { group_id: group, message: attributes_for(:message) }}.to change(Message, :count).by(1)
+          expect{post :create, params: { group_id: group, message: attributes_for(:message) }}.to change(Message, :count).by(1)
         end
         # 意図した画面に遷移しているか
         it 'redirects to messages#index' do
-         post :create, params: { group_id: group, message: attributes_for(:message) }
-         expect(response).to redirect_to group_messages_path
+          post :create, params: { group_id: group, message: attributes_for(:message) }
+          expect(response).to redirect_to group_messages_path
         end
       end
 # ログインしているが、保存に失敗した場合
@@ -62,26 +62,25 @@ describe MessagesController do
           login_user user
         end
         # メッセージの保存は行われなかったか
-         it 'unsave the new message' do
-          expect{
-          post :create, params: { group_id: group, message: attributes_for(:message, body: nil, image: nil) }
-        }.to change(Message, :count).by(0)
-         end
+          it 'unsave the new message' do
+            expect{
+            post :create, params: { group_id: group, message: attributes_for(:message, body: nil, image: nil) }}.to change(Message, :count).by(0)
+          end
         # 意図したビューが描画されているか
-         it 'redirects to group_messages_path' do
-           post :create, params: { group_id: group, message: attributes_for(:message) }
-           expect(response).to redirect_to group_messages_path
-         end
+          it 'redirects to group_messages_path' do
+            post :create, params: { group_id: group, message: attributes_for(:message) }
+            expect(response).to redirect_to group_messages_path
+          end
       end
 # ログインしていない場合
-       context 'user is not log-in' do
+      context 'user is not log-in' do
         before do
           get :index, params: {group_id: group}
         end
         # 意図した画面にリダイレクトできているか
-         it 'redirects to sign-in page'do
-           expect(response).to render_template :index
-        end
+          it 'redirects to sign-in page'do
+            expect(response).to render_template :index
+          end
        end
     end
 end
